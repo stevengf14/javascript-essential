72a8cf01c92ab3e31e745ca4e35c1d9530a59a16
Steven <stevengf14@gmail.com>
2022-04-21 15:44:21 -0500

    Numbers

--- js/app.js
+++ js/app.js
@@ -1,16 +1,7 @@
 "use strict"
 
-var myName = 'Steven';
-console.log(myName);
-const pi = 3.1416;
-
-function hello() {
-    let myName = 'Ohter name';
-    console.log(myName);
-    let myAge = 27;
-    console.log(myAge);
-    // pi = 15
-    console.log(pi);
-}
-
-hello();
+var age = 27;
+var amount = "20";
+var newValue = Number(amount);
+parseInt();
+parseFloat();
