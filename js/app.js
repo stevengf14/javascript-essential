98bd795d718774150720f305479c0b30341863e6
Steven <stevengf14@gmail.com>
2022-04-21 16:05:25 -0500

    JSON

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 "use strict"
 
-var symbol1 = Symbol();
-var symbol2 = Symbol();
-var environment = Symbol('dev');
+// JSON => Javascript Object Notation
+var person = { name: 'Figueroa', email: 'figueroa@email.com' };
+
+var people = [
+    { name: 'Steven', email: 'steven@email.com' },
+    { name: 'Andres', email: 'andres@email.com' },
+    { name: 'Guaman', email: 'guaman@email.com' },
+    person
+];
+
+var peopleJSON = JSON.stringify(person);
+var newPeople = JSON.parse(peopleJSON);
+var x = false;
