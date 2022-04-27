9d9fc5eaa71109944dbd43284c8da1aa9e74cc40
Steven <stevengf14@gmail.com>
2022-04-27 14:56:00 -0500

    Arithmetic Operators

--- js/app.js
+++ js/app.js
@@ -13,3 +13,40 @@ var people = [
 var peopleJSON = JSON.stringify(person);
 var newPeople = JSON.parse(peopleJSON);
 var x = false;
+
+// ******************************
+// **** Arithmetic Operators ****
+
+var dataA = 10;
+var dataB = 20;
+
+// Addition
+var addition = dataA + dataB;
+console.log('The addition of ' + dataA + ' + ' + dataB + ' = ' + addition);
+
+// Substraction
+var substraction = dataA - dataB;
+console.log('The substraction of ' + dataA + ' - ' + dataB + ' = ' + substraction);
+
+// Multiplication
+var multiplication = dataA * dataB;
+console.log('The multiplication of ' + dataA + ' * ' + dataB + ' = ' + multiplication);
+
+// Division
+var division = dataA / dataB;
+console.log('The division of ' + dataA + ' / ' + dataB + ' = ' + division);
+
+// Module
+var module = dataA % dataB;
+console.log('The module of ' + dataA + ' % ' + dataB + ' = ' + module);
+
+// Increment
+var increment = dataA;
+increment++;
+console.log('The increment ++ of ' + dataA + ' = ' + increment);
+
+// decrement
+var decrement = dataA;
+decrement--;
+console.log('The decrement -- of ' + dataA + ' = ' + decrement);
+
